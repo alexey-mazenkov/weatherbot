7a420a30507c0ae57871ad2865830e0f70889270
alexey-mazenkov <mazenkovof@gmail.com>
2019-09-27 22:27:16 +0700

    Import modules.

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 # Weather Bot v0.0.1.
 # Deverloper - Mazenkov Alexey
 
+import pyowm  # Importing module with weather data.
+owm = pyowm.OWM('8e2f4534df1cbcb6300fc8fa009f4f4e')
+
+import telebot  # Importing module with TelegramBot API
+bot = telebot.TeleBot('828439192:AAH-0ngnJwu_4rxZgKTc_CV5RslHTP4puDo')
+
